be7dcb56feb17317ae97b7190bc81f0d41f39401
samuel14find <bicalho.sam@gmail.com>
2020-06-18 21:28:25 -0300

    Fazendo os registro dos services SeederFuncionario, GestaoRepository, MockMailService

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using gestao.Data;
+using gestao.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,11 +16,24 @@ namespace gestao
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            this._config = config;
+
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppGestaoContext>(cfg =>{
+                cfg.UseSqlServer(_config.GetConnectionString("StringConexaoBancoGestao"));
+
+            });
+            services.AddTransient<SeederFuncionario>();
             services.AddControllersWithViews();
+            services.AddScoped<IRepository, GestaoRepository>();
+            services.AddTransient<IMailService, MockMailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +42,8 @@ namespace gestao
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            } else
+            }
+            else
             {
                 app.UseExceptionHandler("/error");
             }
@@ -33,8 +51,9 @@ namespace gestao
             app.UseNodeModules();
             app.UseRouting();
 
-            app.UseEndpoints(cfg => {
-                cfg.MapControllerRoute("Fallback", "{controller}/{action}/{id?}", new {controller = "App", Action="Index"});
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Fallback", "{controller}/{action}/{id?}", new { controller = "App", Action = "Index" });
             });
         }
     }
