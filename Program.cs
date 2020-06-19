0bc5dd00c938c4d3a5e7ff9f633c591afcc36812
samuel14find <bicalho.sam@gmail.com>
2020-06-18 21:27:00 -0300

    Aqui estou trabalhando com a uma novo arquivo de configuração para aplicação, config.json. Trabalhei para que o Seeding do banco funcione antes do servidor web subir

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using gestao.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,16 +13,47 @@ namespace gestao
 {
     public class Program
     {
+        //  Comentários:
+        // Nessa classe vamos usar a SeederFuncionario para "semear" alguns dados no 
+        // banco de dados. Isso é para que possar ter esses dados prontos antes 
+        // servidor web subir. Observar como o truque foi colocar um método entre o 
+        // Build() e o Run().   
+        // Desse modo, toda hora que iniciarmos (dotnet run) a aplicação, vai ocorrer 
+        // a tentativa de "semear" o banco de dados.
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            ExecutaSeeding(host);
+            host.Run();
+        }
+
+        private static void ExecutaSeeding(IHost host)
+        {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using(var scope = scopeFactory.CreateScope())
+            {
+                var seeder = scope.ServiceProvider.GetService<SeederFuncionario>();
+                seeder.Seed();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(SetupConfiguration)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        //  Comentarios
+        // Aqui vamos criar um arquivo de configuração. Com isso ganhamos a habilidade 
+        // de criar outros aquivos de configuração, como um XML se o ambiente onde a 
+        // aplicação for hospedada solicitar. 
+        private static void SetupConfiguration(HostBuilderContext context, IConfigurationBuilder builder)
+        {
+            //Remove opcoes de configuração padrão
+            builder.Sources.Clear();
+            builder.AddJsonFile("config.json", false, true)
+                   .AddEnvironmentVariables();
+        }
     }
 }
