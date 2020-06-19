028378e33564ee03ac22424427eb0d8fe7d06ef8
samuel14find <bicalho.sam@gmail.com>
2020-06-19 20:00:02 -0300

    Adicionei o esse ViewModel para fazer validação na API

---
+++ ViewModels/FichaFuncionalViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace gestao.ViewModels
+{
+    //  Comentários:
+    // Vou usar essa ViewModel para validar dados da API de fichas. O mesmo subsystema
+    // de model validation existe em APIs e quando trabalhamos com views. 
+    // Aqui vou usar as mesmas propriedades que o model, mas posso ter nomes diferentes
+    public class FichaFuncionalViewModel
+    {
+        public int fichaid { get; set; }
+        [Required]
+        [MinLength(5)]
+        public string Titulo { get; set; }
+         [Required]
+        public string  Descricao { get; set; }
+        public DateTimeOffset DataFicha { get; set; }
+    }
+}
