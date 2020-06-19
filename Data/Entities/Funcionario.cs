b9118277d93c08784dd114d97364bc86425cd532
samuel14find <bicalho.sam@gmail.com>
2020-06-19 14:58:18 -0300

    Adicionei propriedade navegacional para FichaFuncional

--- Data/Entities/Funcionario.cs
+++ Data/Entities/Funcionario.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
@@ -46,6 +47,9 @@ namespace gestao.Data.Entities
         
         [DataType(DataType.DateTime)]
         public DateTimeOffset? dataexercicio { get; set; }
+
+
+        public ICollection<FichaFuncional> Fichas {get; set;} // Propriedade navegacional de coleção
     }
     
 }
