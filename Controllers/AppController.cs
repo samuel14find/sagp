d74e427a880444d7752ee6c32a0058f2efda7408
samuel14find <bicalho.sam@gmail.com>
2020-06-18 21:22:20 -0300

    Criei a Action Cadastro com o HttpPost para cadastrar um funcionario. Ainda não lidei com erros que por ventura aparece. Tenho que tratar depois. Usei também um mock de mail service para mostrar mensagem quando o usuário foi cadastro no console

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -1,13 +1,49 @@
+using gestao.Data;
+using gestao.Data.Entities;
+using gestao.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gestao.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+        private readonly IRepository _context;
+        public AppController(IMailService mailService, IRepository context)
+        {
+            this._context = context;
+            this._mailService = mailService;
+
+        }
         public IActionResult Index()
         {
             ViewBag.Title = "Home";
             return View();
         }
+        [HttpGet("Cadastro")]
+        public IActionResult Cadastro()
+        {
+
+            return View();
+        }
+        [HttpPost("Cadastro")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Cadastro([Bind("matricula,nome,cargo,setor,email,datanomeacao,dataposse,dataexercicio")] Funcionario funcionario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(funcionario);
+                _context.Commit();
+                _mailService.SendMessage($"O funcionário {funcionario.nome}, cuja matrícula é {funcionario.matricula}, e o email {funcionario.email} foi cadastrado no sistema. Entrou em exercicio em {funcionario.dataexercicio}");
+                ViewBag.UserMessage = "Usuário Cadastrado com Sucesso!";
+                ModelState.Clear();
+                //return RedirectToAction(nameof(Index));
+            }
+            else {
+                //verificar erros
+            }
+            return View();
+        }
+
     }
 }
