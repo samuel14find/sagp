12c53d87aa57cc8df77774460f764c5965125e42
samuel14find <bicalho.sam@gmail.com>
2020-06-19 22:16:25 -0300

    Alteração do controller Funcionarios para trabalhar com mapper, implantação do post para cadastrar funcionario

--- Controllers/FuncionariosController.cs
+++ Controllers/FuncionariosController.cs
@@ -1,76 +1,111 @@
 using System;
 using System.Collections.Generic;
+using AutoMapper;
 using gestao.Data;
 using gestao.Data.Entities;
+using gestao.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace gestao.Controllers
 {
-    //  Comentarios:
-    // Essa classe será um controler para "expor" um API endpoint. Lembrando que no 
-    // ASP.NET Core não tem mais uma biblioteca expecífica para APIs. Agora fica tudo
-    // na biblioteca Controller ou ControllerBase. 
-    // O atributo [ApiController] ajuda na execução de ferramentas de documentação de API
-    // deixando elas saberem que o controller é uma api. 
-    // E também o atributo [Produces] também vai ajudar nessas ferramentas, dizendo que esse 
-    // API controller vai sempre retornar um Json. 
-    [Route("api/[Controller]")]
-    [ApiController]
-    [Produces("application/json")]
+     [Route("api/[Controller]")]
     public class FuncionariosController: Controller
     {
         private readonly IRepository _repository;
-        private readonly ILogger<FuncionariosController> _logger;
+        private readonly ILogger<FichasController> _logger;
+        private readonly IMapper _mapper;
 
-        public FuncionariosController(IRepository repository, ILogger<FuncionariosController> logger)
+        public FuncionariosController(IRepository repository, ILogger<FichasController> logger, IMapper mapper)
         {
             this._repository = repository;
             this._logger = logger;
+            this._mapper = mapper;
         }
 
-        //  Comentários:
-        // Aqui vou permitir a negociação do conteúdo. O truque é retornar um ActionResult.
-        // Por padrão o MVC 6 retorna JSON. Mas posso mudar isso a adicionar outras formatos de retorno.
-        // Retornando um OK(), ou dizendo que o Status Code é o 200. O Ok() está ligado ao 200
-        // e o BadRequest está ligado ao 400.
-        // Aqui temos a questão de documentar a api, utilizando ferramentas como Swagger. Por 
-        // isso estou colocando o type ActionResult e não IActionResult. O IActionResult esconde
-        // o tipo de dado real que está sendo retornado. Assim estamos definindo o tipo de retorno 
-        // que é um IEnumerable do tipo Funcionario. A ideia é deixar o código mais "limpo" ou consiso 
-        // que permite executar ferramentas para ajudar a documentar a API. 
-        // ATENÇÃO. Os atributos [ProducesResponseType] necessitaram que eu fosse lá na 
-        // classe Startup e adicionasse um SetCompatibilityVersion.
-        // Com esses atributos estou dizendo: "Dessa api eu espero esses dois tipos de respnse"
-        [HttpGet]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        public ActionResult<IEnumerable<Funcionario>> Get()
+         [HttpGet]
+        public IActionResult Get()
         {
             try
             {
-
-            return Ok(_repository.GetFuncionarios());
-            } catch(Exception ex)
+                // Comentarios:
+                // Ao usar o Mapper, se for mapear collections, a dica é mapear 
+                // de collection para collection, de list para list etc. 
+                return Ok(_mapper.Map<IEnumerable<Funcionario>, IEnumerable<FuncionarioViewModel>>(_repository.GetFuncionarios()));
+            }
+            catch (Exception ex)
             {
-                _logger.LogError($"Falha em obter os funcionarios: {ex}");
-                return BadRequest("Falha em obter os funcionarios");
+                _logger.LogError($"Falha ao buscar os funcionarios: {ex}");
+                return BadRequest("Falha ao buscar os funcionarios");
             }
         }
-        [HttpGet("{matricula}")]
-        public IActionResult Get(string matricula)
+
+         [HttpGet("{id}")]
+        public IActionResult Get(int id)
         {
-            try 
+             try
             {
-                var funcionario = _repository.GetFuncionarioPorMatricula(matricula);
-                if(funcionario != null) return Ok(funcionario);
+                var funcionario = _repository.GetFuncionarioPorId(id);
+                //  Comentário:
+                // Isso aqui funciona assim: Pega a ficha que estão passando e retorna uma versão 
+                // mapeada ao FichaFuncionalViewModel visto que sempre vamos querer retornar uma view model
+                if(funcionario != null) return Ok(_mapper.Map<Funcionario, FuncionarioViewModel>(funcionario));
                 else return NotFound();
             }
             catch (Exception ex)
             {
-                _logger.LogError($"Falha em obter o funcionarios: {ex}");
-                return BadRequest("Falha em obter o funcionarios");
+                _logger.LogError($"Falha ao buscar o funcionario: {ex}");
+                return BadRequest("Falha ao buscar o funcionario");
             }
         }
+
+        //  Comentarios
+        // A minha intenção aqui é estruturar o "body" da requisição com os dados 
+        // que quero salvar. Por isso usei o atributo [FromBody].
+        // Observe que se consegui adicionar a entidade com sucesso, vou retornar
+        // Created(). Assim estamos em sintonia com o jeito de trabalhar do Http
+        // Com Post, se estamos criando um objeto, o correto é retornar esse Created.
+        // O status de created é 201. 
+        // Observe aqui que justamente por estar usando o ViewModel vou usar o ModelState 
+        // para validar as coisas. 
+        [HttpPost]
+        public IActionResult Post([FromBody]FuncionarioViewModel model)
+        {
+        
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    var novoFuncionario = _mapper.Map<FuncionarioViewModel, Funcionario>(model);
+                    //  Comentários:
+                    // Aqui vou fazer uma pequena validação. Verifica se 
+                    // não vou passado data alguma. Isso porque não passei 
+                    // required na data lá no FichaFuncionalViewModel.
+
+                    if( novoFuncionario.dataexercicio == DateTimeOffset.MinValue)
+                    {
+                        novoFuncionario.dataexercicio = DateTimeOffset.UtcNow;
+                    };
+                _repository.AdicionarEntidade(novoFuncionario);
+                _repository.Commit();
+
+                //  Comentários:
+                // Como eu convertir o mode para novaFicha, agora faço o trabalho 
+                // o contrário. Mas observe que para isso funcionar, ir lá no profile e 
+                // colocar ReverseMap.
+                return Created($"/api/funcionarios/{novoFuncionario.FuncionarioId}", _mapper.Map<Funcionario,FuncionarioViewModel>(novoFuncionario));
+                } else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Falha ao tentar salvar no banco: {ex}");
+            }
+            return BadRequest("Falha ao salva ficha!");
+        }
+
     }
+
 }
