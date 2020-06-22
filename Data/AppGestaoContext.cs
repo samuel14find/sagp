d64980f3c707dc01290482f9e87d6cac2550d0bf
samuel14find <bicalho.sam@gmail.com>
2020-06-22 16:25:12 -0300

    Configurei o contexto para herdar de IdentityDbContext usando como tipo a classe IdentityUser

--- Data/AppGestaoContext.cs
+++ Data/AppGestaoContext.cs
@@ -1,10 +1,16 @@
 using gestao.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace gestao.Data
 {
-    public class AppGestaoContext: DbContext
+    //  Comentário:
+    // Aqui vou trabalhar com o Identity do Asp Net Core. Esse IdentityUser representa o usuário
+    // e ele já tem várias propriedades prontas para usar como Email, UserName, Passowrd etc. 
+    // É possível criar uma Classe com algumas propriedades que queremos adicionar, e herdar as do 
+    // IdentityUser. 
+    public class AppGestaoContext: IdentityDbContext<IdentityUser>
     {
         public AppGestaoContext(DbContextOptions<AppGestaoContext> options): base(options)
         {
