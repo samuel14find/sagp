548f2eb9df5c2a0f83d25f2e76a8a468b7f2ec06
samuel14find <bicalho.sam@gmail.com>
2020-06-22 16:29:48 -0300

    Coloquei a aplicação para usar o Identity para gerenciar o login, register e logout. Arquivos criados pelo Scallfold

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using gestao.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(gestao.Areas.Identity.IdentityHostingStartup))]
+namespace gestao.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
