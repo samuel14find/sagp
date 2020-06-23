796a6ba756abd572cb809a2cd7bbcd8cc1f0c55f
samuel14find <bicalho.sam@gmail.com>
2020-06-23 14:46:50 -0300

    Adiciei um Service para capturar os dados do banco de dados usando a api que crieo com o dotnet core. Fiz uma interface para fazer um Type Safety.

---
+++ ClientApp/app/Shared/Funcionario.ts
@@ -0,0 +1,12 @@
+export interface Funcionario {
+
+        funcionarioid: number;
+        matricula: string;
+        nome: string;
+        cargo: string;
+        setor: string;
+        email: string;
+        datanomeacao: Date;
+        dataposse: Date;
+        dataexercicio: Date;
+}
