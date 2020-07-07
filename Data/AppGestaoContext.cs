4b84e94ff14fd4684fc0e052e6f13ea611cf33ec
samuel14find <bicalho.sam@gmail.com>
2020-07-07 15:57:51 -0300

    Adição de propriedades para trabalhar com Ficha, Carrinho de ficha, progressões e carreira

--- Data/AppGestaoContext.cs
+++ Data/AppGestaoContext.cs
@@ -12,11 +12,21 @@ namespace gestao.Data
     // IdentityUser. 
     public class AppGestaoContext: IdentityDbContext<IdentityUser>
     {
+        // public AppGestaoContext()
+        // {
+        // }
+
         public AppGestaoContext(DbContextOptions<AppGestaoContext> options): base(options)
         {
 
         }
         public DbSet<Funcionario> Funcionarios { get; set; }
         public DbSet<FichaFuncional> Fichas { get; set; }
+        public DbSet<FichaDetalhe> FichaDetalhes { get; set; }
+        public DbSet<CarrinhoFichaItem> CarrinhoFichaItens {get; set;}
+        public DbSet<Carreira> Carreiras {get; set;}
+        public DbSet<ProgressaoCarreira> Progressoes {get; set;}
+        public DbSet<FuncionarioCarreira> FuncionariosCarreira{get; set;}
+
     }
 }
