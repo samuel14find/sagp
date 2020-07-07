1670919e7e0b2ab73c0e8715d3544cbc669ed0d5
samuel14find <bicalho.sam@gmail.com>
2020-07-07 16:02:18 -0300

    Interface do Repositoru do Ficha funcional

---
+++ Data/IFichaFuncionalRepository.cs
@@ -0,0 +1,13 @@
+﻿using gestao.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gestao.Data
+{
+    public interface IFichaFuncionalRepository
+    {
+        void CriarFicha(FichaFuncional ficha);
+    }
+}
