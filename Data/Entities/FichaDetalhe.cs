6b4827433f8a117476eceb43fbbfd23a382a47f0
samuel14find <bicalho.sam@gmail.com>
2020-07-14 15:25:08 -0300

    Alteração de propriedades e aqui usei o Lazy Loading do EF core.

--- Data/Entities/FichaDetalhe.cs
+++ Data/Entities/FichaDetalhe.cs
@@ -6,13 +6,12 @@ namespace gestao.Data.Entities
     public class FichaDetalhe
     {
         public int FichaDetalheId { get; set; }
-        public int fichafuncId { get; set; }
+
+        public int FichaId { get; set; }
         public int FuncionarioId { get; set; }
 
-        [ForeignKey("FuncionarioId")]
-        public Funcionario Funcionario { get; set; }
-
-        [ForeignKey("fichafuncId")]
-        public FichaFuncional FichaFuncional{get; set;}
+        public virtual Funcionario Funcionario { get; set; }
+        
+        public virtual Ficha Ficha {get; set;}
     }
 }
